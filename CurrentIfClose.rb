b0aeecb15031ca1066e135d317174b2507b5e1b7
Gommer <go.st@web.de>
2013-02-06 00:18:48 +0100

    poty added
    added rotary poty to enable/disable dIR functionality

--- CurrentIfClose.rb
+++ CurrentIfClose.rb
@@ -4,18 +4,23 @@
 require 'tinkerforge/ip_connection'
 require 'tinkerforge/bricklet_distance_ir'
 require 'tinkerforge/bricklet_dual_relay'
+require 'tinkerforge/bricklet_rotary_poti'
 
 include Tinkerforge
 
-HOST = '192.168.1.33' # IP Address of the wlan brick
+HOST = '127.0.0.1' # IP Address of the wlan brick
 PORT = 4223
 UID_dIR = 'cJb' # Unique ID of the distance IR brick
 UID_dRL = 'c2W' # Unique ID of the dual relay brick
+UID_rp  = 'cDY' # Unique ID of the dual relay brick
 
 ipcon = IPConnection.new # Create IP connection
 
 dir = BrickletDistanceIR.new UID_dIR, ipcon # Create device object
 drl = BrickletDualRelay.new UID_dRL, ipcon
+rp  = BrickletRotaryPoti.new UID_rp, ipcon
+
+active = false
 
 ipcon.connect HOST, PORT # Connect to brickd
 # Don't use device before ipcon is connected
@@ -24,19 +29,26 @@ ipcon.connect HOST, PORT # Connect to brickd
 #dir.set_debounce_period 10000
 
 dir.set_distance_callback_period 200
+rp.set_position_callback_period 200
 
 # Register threshold reached callback for distance smaller than 20cm
 dir.register_callback(BrickletDistanceIR::CALLBACK_DISTANCE_REACHED) do |distance|
   puts "Distance is smaller than 20cm: #{distance/10.0} cm"
 end
 
-# Configure threshold for "smaller than 20cm" (unit is mm)
-#dir.set_distance_callback_threshold '<', 200, 0
+rp.register_callback(BrickletRotaryPoti::CALLBACK_POSITION) do |position|
+  if position > 0
+    active = true
+  else
+  	active = false
+  end
+  puts "Position: #{position}"
+end
 
 # Register distance callback (parameter has unit mm)
 dir.register_callback(BrickletDistanceIR::CALLBACK_DISTANCE) do |distance|
     #puts "Distance: #{distance/10.0} cm"
-    if distance/10.0 < 20
+    if distance/10.0 < 20 && active 
         drl.set_state true, false
     else
         drl.set_state false, false
